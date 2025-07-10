dee5de9f405810a26a168f07e311d2ab6452c12d
ErickCs <erickj.cruzs@gmail.com>
2025-07-10 11:46:07 -0400

    Adding Testimonials

---
+++ src/components/Home/Testimonials.tsx
@@ -0,0 +1,12 @@
+export const Testimonials = () => {
+  return (
+    <section>
+      <div className="container mx-auto">
+        <div className="space-y-10 text-center">
+          <h2 className="text-5xl font-bold">Testimonials</h2>
+          <hr className="border-secondaryColor mx-auto my-3 w-[120px] border-2 outline-none" />
+        </div>
+      </div>
+    </section>
+  );
+};
