95eb3cc2d2c33689305b83ee51a6de79a4529176
ErickCs <erickj.cruzs@gmail.com>
2025-07-14 12:30:58 -0400

    Adding METADATA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,26 @@ export const metadata: Metadata = {
   icons: {
     icon: "/images/cts-favicon.png",
   },
+
   title: "Caribbean Temporary Services LLC",
   description:
     "Caribbean Temporary Services, LLC. (CTS), es una empresa local establecida en 1983 con la misión de proveer amplias oportunidades de empleo, mientras proporciona un servicio que maneja las necesidades y contribuye al crecimiento de sus clientes en Puerto Rico.",
+  openGraph: {
+    title: "Caribbean Temporary Services LLC",
+    description:
+      "Caribbean Temporary Services, LLC. (CTS), es una empresa local establecida en 1983 con la misión de proveer amplias oportunidades de empleo, mientras proporciona un servicio que maneja las necesidades y contribuye al crecimiento de sus clientes en Puerto Rico.",
+    type: "website",
+    locale: "es",
+    siteName: "Caribbean Temporary Services LLC",
+    images: [
+      {
+        url: "https://stagingctspr.axesawebhosting9.net/wp-content/uploads/2025/07/cts-brand.webp",
+        width: 1200,
+        height: 630,
+        alt: "Caribbean Temporary Services LLC",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
