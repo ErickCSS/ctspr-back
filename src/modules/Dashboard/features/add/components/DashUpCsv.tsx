0d7e930fa5cfc3222252ea420cac1bd1e9a8c6dd
ErickCs <erickj.cruzs@gmail.com>
2025-10-02 16:25:28 -0400

    fix: update Spanish import button label from 'Importar a Supabase' to 'Importar Empleos'

--- src/modules/Dashboard/features/add/components/DashUpCsv.tsx
+++ src/modules/Dashboard/features/add/components/DashUpCsv.tsx
@@ -175,7 +175,7 @@ export function UploadEmployeesCSV({ user }: { user: User }) {
                       d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
                     />
                   </svg>
-                  Importar a Supabase
+                  Importar Empleos
                 </>
               )}
             </Button>
